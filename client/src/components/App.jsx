07155281d726b3374cc6a18d723948df3932b52c
SONG336 <songziye960123@gmail.com>
2021-12-03 21:10:44 -0500

    Add form on app componnet, add modal view on modal component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,83 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+import { useState } from 'react';
+import Modal from './Modal.jsx';
 
 const App = () => {
+  const [modal, setModal] = useState(false);
 
+  const toggleMoal = () => {
+    setModal(!modal)
+  }
+
+  //console.log(modal);
   return(
-    <h1>hello</h1>
+    <div>
+    <Title>
+      <h1>Hmm... 🤔 Where should I eat?</h1>
+    </Title>
+    <FormConatiner>
+      <Form>
+        <label> Location: <input type="text" name="location"/> </label>
+        <label> Category: <input type="text" name="category"/> </label>
+        <label> Ratings:
+        <select>
+          <option value="ratings">ratings</option>
+          <option value="5">5 </option>
+          <option value="4">4 + </option>
+          <option value="3">3 +</option>
+          <option value="2">2 +</option>
+      </select>
+      </label>
+      <label> Price:
+        <select>
+          <option value="price">price</option>
+          <option value="$">$ </option>
+          <option value="$$">$$  </option>
+          <option value="$$$">$$$ </option>
+      </select>
+      </label>
+      </Form>
+      <h2>Let's find good place for you!</h2>
+      <SearchButton onClick={ () => setModal(!modal)}>Search</SearchButton>
+    </FormConatiner>
+
+    {modal ? <Modal toggleMoal={toggleMoal}/> : ''}
+
+    </div>
+
   )
 }
 
 export default App;
+
+const Title = styled.div`
+  padding-top: 50px;
+  width: 100%;
+  height: 200px;
+  background-color: white;
+  display: flex;
+  justify-content: center;`
+
+const FormConatiner = styled.div`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 300px;
+  border: solid;
+`
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin: 20px;
+`
+
+const SearchButton = styled.button`
+  height: 30px;
+  width: 100px;
+`
+
---
+++ client/src/components/Modal.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+import { useState } from 'react';
+
+const Modal = (props) => {
+  //console.log(props);
+  return (
+    <div>
+      <Background>
+        <ModalWrapper>
+          <CloseButton>
+            <button onClick={() => { props.toggleMoal()}}>X</button>
+          </CloseButton>
+
+        </ModalWrapper>
+      </Background>
+    </div>
+  )
+
+}
+
+export default Modal;
+
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  background: #fff9f4;
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  z-index: 9999
+`;
+
+const ModalWrapper = styled.div`
+  width: 900px;
+  height: 400px;
+  border-radius: 12px;
+  background-color: white;
+  box-shadow: #f6f7fb 0px 5px 15px;
+  display: flex;
+  flex-direction: column;
+  padding: 25px;
+  z-index: 9999
+`;
+
+const CloseButton = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
